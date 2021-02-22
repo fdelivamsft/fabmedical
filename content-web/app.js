39964a113bd3fe22bad48be7de0da331ebb6e157
fdeliva <fdeliva@microsoft.com>
2021-02-22 04:54:13 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("5e219149-26f9-4835-8ddd-80047acfad34");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
